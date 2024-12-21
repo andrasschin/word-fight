8c3072d4d5af678a89830fd36f4a3bf4ce70f0a9
itsmeeandrew <schin.andras01@gmail.com>
2024-12-21 19:45:25 +0100

    update word endpoint

--- server/classes/APIHandler.js
+++ server/classes/APIHandler.js
@@ -1,71 +1,70 @@
 const fetch = require("node-fetch");
 
 class APIHandler {
-    static randomWordsAPIBase = "https://random-word-api.herokuapp.com/word";
-    static dictionaryAPIBase = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-    static badRequestMessage = "No Definitions Found";
+  static randomWordsAPIBase = "https://random-word-api.vercel.app/api?words=1";
+  static dictionaryAPIBase = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  static badRequestMessage = "No Definitions Found";
 
-    static fetchRandomWord() {
-        const path = this.randomWordsAPIBase;
-        
-        return new Promise((resolve, reject) => {
-            fetch(path)
-                .then(res => {
-                    const word = res.json();
-                    resolve(word);
-                })
-                .catch(err => {
-                    console.log("[ERROR] fetchRandomWord: ", err);
-                    reject(err);
-                })
+  static fetchRandomWord() {
+    const path = this.randomWordsAPIBase;
+
+    return new Promise((resolve, reject) => {
+      fetch(path)
+        .then((res) => {
+          const word = res.json();
+          resolve(word);
+        })
+        .catch((err) => {
+          console.log("[ERROR] fetchRandomWord: ", err);
+          reject(err);
         });
+    });
+  }
+
+  static fetchDefiniton(word) {
+    const path = this.dictionaryAPIBase + word;
+
+    return new Promise((resolve, reject) => {
+      fetch(path)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          if (data.title && data.title === APIHandler.badRequestMessage) {
+            resolve(-1);
+          } else {
+            const definition = data[0].meanings[0].definitions[0].definition;
+            resolve(definition);
+          }
+        })
+        .catch((err) => {
+          console.log("[ERROR] fetchDefiniton: ", err);
+          reject(err);
+        });
+    });
+  }
+
+  static async getWordWithDefinition() {
+    const word = await this.fetchRandomWord();
+    const definition = await this.fetchDefiniton(word);
+
+    if (definition === -1) return this.getWordWithDefinition();
+
+    return {
+      word,
+      definition,
+    };
+  }
+
+  static async getWordsWithDefinitions(n) {
+    let wordsWithDefinitionsArr = [];
+
+    for (let i = 0; i < n; i++) {
+      wordsWithDefinitionsArr.push(this.getWordWithDefinition());
     }
 
-    static fetchDefiniton(word) {
-        const path = this.dictionaryAPIBase + word;
-
-        return new Promise((resolve, reject) => {
-            fetch(path)
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    if (data.title && data.title === APIHandler.badRequestMessage) {
-                        resolve(-1);
-                    } else {
-                        const definition = data[0].meanings[0].definitions[0].definition;
-                        resolve(definition);
-                    }
-                })
-                .catch(err => {
-                    console.log("[ERROR] fetchDefiniton: ", err);
-                    reject(err);
-                })
-            }
-        )
-    }
-
-    static async getWordWithDefinition() {
-        const word = await this.fetchRandomWord();
-        const definition = await this.fetchDefiniton(word);
-        
-        if (definition === -1) return this.getWordWithDefinition();
-
-        return {
-            word,
-            definition
-        };
-    }
-
-    static async getWordsWithDefinitions(n) {
-        let wordsWithDefinitionsArr = [];
-
-        for (let i = 0; i < n; i++) {
-            wordsWithDefinitionsArr.push(this.getWordWithDefinition());
-        }
-
-        return Promise.all(wordsWithDefinitionsArr);
-    }
+    return Promise.all(wordsWithDefinitionsArr);
+  }
 }
 
 module.exports = APIHandler;
--- server/classes/Word.js
+++ server/classes/Word.js
@@ -1,22 +1,22 @@
 class Word {
-    static hintRatio = 1/3;
+  static hintRatio = 2 / 5;
 
-    constructor (word, definition) {
-        this.word = word;
-        this.definition = definition;
+  constructor(word, definition) {
+    this.word = word;
+    this.definition = definition;
+  }
+
+  getHint() {
+    const hint = new Array(this.word.length).fill("_");
+    const numberOfHintedLetters = Math.round(this.word.length * Word.hintRatio);
+
+    for (let i = 0; i < numberOfHintedLetters; i++) {
+      let idx = Math.floor(Math.random() * this.word.length);
+      if (hint[idx] === "_") hint[idx] = this.word[idx];
+      else i--;
     }
-
-    getHint() {
-        const hint = new Array(this.word.length).fill("_");
-        const numberOfHintedLetters = Math.round(this.word.length * Word.hintRatio);
-
-        for (let i = 0; i < numberOfHintedLetters; i++) {
-            let idx = Math.floor(Math.random() * this.word.length);
-            if (hint[idx] === "_") hint[idx] = this.word[idx];
-            else i--;
-        }
-        return hint.join(" ");
-    }
+    return hint.join(" ");
+  }
 }
 
 module.exports = Word;
